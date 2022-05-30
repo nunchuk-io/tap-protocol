04c0ffb046fb96e5d56cdce98ce768504dca7d8c
giahuy98 <giahuy@nunchuk.io>
2022-05-30 09:54:11 +0700

    Update test transport iOS

--- tests/transport_test.cpp
+++ tests/transport_test.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include "emulator.h"
+#include "tap_protocol/tap_protocol.h"
 #include "tap_protocol/transport.h"
 
 using json = nlohmann::json;
@@ -59,11 +60,41 @@ TEST_CASE("decode cbor ok") {
 }
 
 TEST_CASE("ios transport") {
-  std::unique_ptr<tap_protocol::Transport> tp =
-      std::make_unique<CardEmulator>();
-  tap_protocol::TapSigner tapSigner(std::move(tp));
+  json j = json::parse(R"(
+{
+  "address": "",
+  "birth": 700001,
+  "card_nonce": {
+    "bytes": [220, 181, 216, 210, 239, 27, 50, 31, 206, 173, 55, 127, 98, 97, 229, 71],
+    "subtype": null
+  },
+  "path": [
+    2147483732,
+    2147483648,
+    2147483648
+  ],
+  "proto": 1,
+  "pubkey": {
+    "bytes": [3, 50, 131, 14, 50, 9, 233, 80, 149, 122, 211, 150, 76, 34, 63, 136, 248, 223, 97, 218, 210, 247, 22, 8, 127, 92, 51, 109, 166, 51, 114, 165, 110],
+    "subtype": null
+  },
+  "tapsigner": true,
+  "ver": "0.1.0"
+}
+)");
 
-  auto st = tapSigner.Status();
+  auto sendReceiveFuncIOS = [=](const tap_protocol::APDURequest& req) {
+    MESSAGE("cla = ", int(req.cla));
+    MESSAGE("ins = ", int(req.ins));
+    MESSAGE("p1 = ", int(req.p1));
+    MESSAGE("p2 = ", int(req.p2));
+    auto data = json::to_cbor(j);
+    // Two more sw bytes
+    return tap_protocol::APDUResponse{.data = data, .sw1 = 0x90, .sw2 = 0x00};
+  };
 
-  MESSAGE(json(st).dump(2));
+  auto tp = tap_protocol::MakeDefaultTransportIOS(sendReceiveFuncIOS);
+  auto resp = tp->Send({{"cmd", "status"}});
+
+  CHECK(resp == j);
 }
